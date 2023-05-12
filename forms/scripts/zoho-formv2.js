6e91adaae47117c8bd6e8e1d792b2e036ac6887c
REVREBEL <93675875+RevRebel@users.noreply.github.com>
2023-05-12 01:45:20 -0700

    Create zoho-formv2.js

---
+++ forms/scripts/zoho-formv2.js
@@ -0,0 +1,33 @@
+<script src="https://cdn.jsdelivr.net/gh/RevRebel/revrebel.io/forms/scripts/validation.js">
+</script>
+
+<script type="text/javascript">
+    var Webflow = Webflow || [];
+    Webflow.push(function() {
+        $('input[name=]').val('form');
+        $('input[name=form action]').val('https://forms.zohopublic.com/revrebel/form/RevenueToolkitDownload/formperma/FEfMfXuhNI_uLso_osA9Z-xyv5Zc7EcT2bhXPCEmoEg/htmlRecords/submit');
+        $('input[name=method]').val('POST');
+				$('input [name=zf_referrer_name]').val('website/revenue-toolkit-form');
+	 			$('input[name=zc_gad]').val('');
+	 			$('input[name=utm_source]').val('');
+	 			$('input[name=utm_medium]').val('');
+	 			$('input[name=utm_campaign]').val('');
+	 			$('input[name=utm_term]').val('');
+	 			$('input[name=utm_content]').val('');
+	 			$('input[name=returnURL]').val('http://revrebel.io');
+        //Add things to the form tag for zoho
+        $('#form').attr('accept-charset','UTF-8');
+        $('#form').attr('enctype'='multipart/form-data');
+				$('#form').attr('id'='form');
+        $('#form').submit(function() {
+						javascript:document.charset="UTF-8"; 
+						return zf_ValidateAndSubmit();					
+        });
+
+				var zf_DateRegex = new RegExp("^(([0][1-9])|([1-2][0-9])|([3][0-1]))[-](Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec|JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)[-](?:(?:19|20)[0-9]{2})$");
+				var zf_MandArray = [ "Name_First", "Name_Last", "Email", "SingleLine"]; 
+				var zf_FieldArray = [ "Name_First", "Name_Last", "Email", "SingleLine"]; 
+				var isSalesIQIntegrationEnabled = false;
+				var salesIQFieldsArray = [];
+			}
+</script>
