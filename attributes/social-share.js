5fcc3f223756a5507ee97699d3d8b0c871293b73
Gary Stringham <141124094+RR-Gary-Stringham@users.noreply.github.com>
2024-03-14 21:51:57 -0700

    Update social-share.js

--- attributes/social-share.js
+++ attributes/social-share.js
@@ -1,4 +1,15 @@
+// <script src="https://cdn.jsdelivr.net/gh/REVREBEL/revrebel.io/attributes/social-share.js"></script>
 
+/**
+*
+* I want this Rich Text to be the wrapper of the my content
+* Add this attribute to the parent div wrapping your content
+* Name : ct-readtime-element  Value: target
+*
+* I want to thisText or Text Span to show the time to read content.
+* Name: ct-readtime-element    Value: time
+*
+*/
 
 
 document.addEventListener("DOMContentLoaded", function () {
